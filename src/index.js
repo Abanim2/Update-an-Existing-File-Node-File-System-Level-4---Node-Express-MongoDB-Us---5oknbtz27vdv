29e5af85c7ac8d59b4c77ce06f58c3017ac1bcef
Abanim2 <115773506+Abanim2@users.noreply.github.com>
2023-04-27 11:00:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
+// const fs = require("fs/promises");
+// const fileName = "myfile.txt";
+// const updateFile = async (fileName, fileContent) => {
+//   // write code here
+//   // dont change function name
+  
+// };
+
+// module.exports = updateFile;
+
+
+
+
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
+
 const updateFile = async (fileName, fileContent) => {
-  // write code here
-  // dont change function name
-  
+  try {
+    // read the existing content of the file
+    const existingContent = await fs.readFile(fileName, "utf-8");
+
+    // concatenate the existing content with the new content
+    const updatedContent = existingContent + ", " + fileContent;
+
+    // write the updated content back to the file
+    await fs.writeFile(fileName, updatedContent);
+
+    return updatedContent;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = updateFile;
